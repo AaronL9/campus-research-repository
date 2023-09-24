0f9c0b0cb4be32c533bcdf1f208a7882cb02ccfb
Aaron Lomibao <aaron.lomibao09@gmail.com>
2023-09-24 16:40:03 +0800

    commit

--- frontend/src/components/submit_research/Upload.jsx
+++ frontend/src/components/submit_research/Upload.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Upload() {
+  const [filename, setFileName] = useState(null);
+  const handleUpload = (e) => {
+    const file = e.target.files[0];
+    file ? setFileName(file.name) : null;
+  }
   return (
     <>
       <img
@@ -9,8 +14,9 @@ export default function Upload() {
         alt="upload_icon.svg"
       />
       <span className="submit-research__upload-title">Browse your computer</span>
-      <input className="submit-research__upload-pdf" id="pdf" type="file" name="pdf" />
+      <input className="submit-research__upload-pdf" id="pdf" type="file" name="pdf" onChange={handleUpload} />
       <label className="submit-research__upload-label" htmlFor="pdf">Upload PDF Files</label>
+      {filename && (<p style={{fontSize: "10px"}}>{filename}</p>)}
     </>
   );
 }

d1fdc58152d28a1c29119d593c0fd571bd86228a
Ricarte <narajarg.mmii@gmail.com>
2023-09-29 18:53:43 +0800

    added minor changes

--- frontend/src/pages/admin/Queue.jsx
+++ frontend/src/pages/admin/Queue.jsx
@@ -7,6 +7,9 @@ import "../../assets/css/admin/queue.css"
 export default function Queue() {
   return (
     <>
+      <h1 className="queue__title">
+        Queue
+      </h1>
       <QueueCard />
       <QueueCard />
     </>

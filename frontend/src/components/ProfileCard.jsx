f2b308d508f02ee4d036b59b80db2cec6db2ba0e
Jessica Zarate <jeca.zarate.up@phinmaed.com>
2023-10-06 02:19:58 +0800

    profile card editable

---
+++ frontend/src/components/ProfileCard.jsx
@@ -0,0 +1,182 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+export default function ProfileCard({ profile }) {
+  const { user } = useAuthContext();
+  const [isEditing, setIsEditing] = useState(false);
+  const [info, setInfo] = useState(profile);
+  const [image, setImage] = useState(null);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+  const handleSave = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+    //Save new info here ...
+
+    //Filter out empty string from the works array ...
+    const newWorks = info.works.filter((work) => work.trim() !== "");
+    setInfo({ ...info, works: newWorks });
+  };
+  const handleAddWork = () => {
+    setInfo({ ...info, works: [...info.works, ""] });
+  };
+
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    if (name === "contribution") {
+      const newWorks = [...info.works];
+      newWorks[index] = value;
+      setInfo({ ...info, works: newWorks });
+    } else {
+      setInfo({ ...info, [name]: value });
+    }
+  };
+  const handleImageChange = (e) => {
+    setImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  return (
+    <>
+      {isEditing ? (
+        <form className="userprofile-usercard" onSubmit={handleSave}>
+          <div className="usercard-user">
+            <img
+              src="/svg/profile-decor.svg"
+              alt="decor"
+              className="userprofile-decor"
+            />
+            <div className="usercard__picture">
+              <img
+                src={image || "/svg/profile-large.svg"}
+                alt="userprofile"
+                className="usercard__profile"
+              />
+              <label htmlFor="file-input">
+                <img
+                  src="/svg/cam-btn.svg"
+                  alt="Cam"
+                  className="usercard__camera"
+                />
+              </label>
+              <input
+                id="file-input"
+                type="file"
+                onChange={handleImageChange}
+                className="picture-file"
+              />
+            </div>
+            <h2>{user?.name}</h2>
+            <Link to="/student/create">
+              <button className="usercard-top__report-btn">
+                <img src="/svg/pen-icon.svg" alt="report_btn" />
+                Research
+              </button>
+            </Link>
+          </div>
+          <div className="usercard-info">
+            <img
+              src="/svg/profilecard-save.svg"
+              alt="save"
+              className="usercard-save"
+              onClick={handleSave}
+            />
+            <div className="usercard-info__bio">
+              <span>Bio:</span>
+              <textarea
+                name="biodata"
+                value={info.biodata}
+                onChange={handleInputChange}
+                maxLength={250}
+              />
+            </div>
+            <div className="usercard-info__address">
+              <span>Research Contribution:</span>
+              {info.works.map((work, index) => (
+                <input
+                  key={index}
+                  name="contribution"
+                  value={work}
+                  onChange={(e) => handleInputChange(e, index)}
+                />
+              ))}
+              <button type="button" onClick={handleAddWork}>
+                Add
+              </button>
+            </div>
+            <div className="usercard-info__contact">
+              <span>Contact:</span>
+              <div className="usercard-info__contact-wrap">
+                <label>
+                  <img src="/svg/mail.svg" alt="email-light" />
+                  <p>{user?.email}</p>
+                </label>
+                <label>
+                  <img src="/svg/telephone.svg" alt="telephone" />
+                  <p> (63+) 923-101-0012</p>
+                </label>
+              </div>
+            </div>
+          </div>
+        </form>
+      ) : (
+        <div className="userprofile-usercard">
+          <div className="usercard-user">
+            <img
+              src="/svg/profile-decor.svg"
+              alt="decor"
+              className="userprofile-decor"
+            />
+            <div className="usercard__picture">
+              <img
+                src="/svg/profile-large.svg"
+                alt="userprofile"
+                className="usercard__profile"
+              />
+            </div>
+            <h2>{user?.name}</h2>
+            <Link to="/student/create">
+              <button className="usercard-top__report-btn">
+                <img src="/svg/pen-icon.svg" alt="report_btn" />
+                Research
+              </button>
+            </Link>
+          </div>
+          <div className="usercard-info">
+            <img
+              src="/svg/profilecard-edit.svg"
+              alt="edit"
+              className="usercard-edit"
+              onClick={handleEdit}
+            />
+            <div className="usercard-info__bio">
+              <span>Bio:</span>
+              <p>{info.biodata}</p>
+            </div>
+            <div className="usercard-info__address">
+              <span>Research Contribution:</span>
+              {info.works.map((work, index) => (
+                <p key={index}>{work}</p>
+              ))}
+            </div>
+            <div className="usercard-info__contact">
+              <span>Contact:</span>
+              <div className="usercard-info__contact-wrap">
+                <label>
+                  <img src="/svg/mail.svg" alt="email-light" />
+                  <p>{user?.email}</p>
+                </label>
+                <label>
+                  <img src="/svg/telephone.svg" alt="telephone" />
+                  <p> (63+) 923-101-0012</p>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- frontend/src/pages/student/UserProfile.jsx
+++ frontend/src/pages/student/UserProfile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ProfileCard from "../../components/ProfileCard";
 import "../../assets/css/userprofile.css";
 import "../../assets/css/news.css";
 import NewResearch from "../../components/news/NewResearch";
@@ -7,7 +8,7 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 const UserProfile = () => {
   const { user } = useAuthContext();
   const [userResearches, setUserResearches] = useState(null);
-  
+
   useEffect(() => {
     if (!user) {
       return;
@@ -34,60 +35,12 @@ const UserProfile = () => {
     <div className="userprofile">
       <h1>Profile</h1>
       <div className="userprofile-content">
-        <div className="userprofile-usercard">
-          <div className="usercard-user">
-            <img
-              src="/svg/profile-decor.svg"
-              alt="decor"
-              className="userprofile-decor"
-            />
-            <div className="usercard__picture">
-              <img
-                src="/svg/profile-large.svg"
-                alt="userprofile"
-                className="usercard__profile"
-              />
-              <img
-                src="/svg/cam-btn.svg"
-                alt="Cam"
-                className="usercard__camera"
-              />
-            </div>
-            <h2>{user?.userName}</h2>
-            <button className="usercard-top__report-btn">
-              <img src="/svg/pen-icon.svg" alt="report_btn" />
-              Research
-            </button>
-          </div>
-          <div className="usercard-info">
-            <div className="usercard-info__bio">
-              <span>Bio:</span>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex.
-              </p>
-            </div>
-            <div className="usercard-info__address">
-              <span>Research Contribution:</span>
-              <p>Research Campus Repository A.Y. 2023-2024</p>
-            </div>
-            <div className="usercard-info__contact">
-              <span>Contact:</span>
-              <div className="usercard-info__contact-wrap">
-                <label>
-                  <img src="/svg/mail.svg" alt="email-light" />
-                  <p>{user?.email}</p>
-                </label>
-                <label>
-                  <img src="/svg/telephone.svg" alt="telephone" />
-                  <p> (63+) 923-101-0012</p>
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ProfileCard
+          profile={{
+            biodata: "The user has not yet written their biodata",
+            works: ["Nothing contributed yet"],
+          }}
+        />
         <div className="userprofile-research">
           <h2>Research History</h2>
           <div className="divider"></div>
